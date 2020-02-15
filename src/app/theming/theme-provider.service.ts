621adae239e635b78e82348a702fb1ccdcec6dab
Andreas Coroiu <andreas.coroiu@gmail.com>
2020-02-15 15:37:59 +0100

    Revert default theme to old theme. Create new theme as alt-theme.

--- src/app/theming/theme-provider.service.ts
+++ src/app/theming/theme-provider.service.ts
@@ -5,6 +5,10 @@ import { HttpClient } from '@angular/common/http';
 
 import less from 'less';
 import { Theme } from './theme';
+import { ActivatedRoute } from '@angular/router';
+
+const urlKey = 'theme';
+const defaultUrl = 'assets/default-theme.json';
 
 @Injectable({
   providedIn: 'root'
@@ -13,37 +17,41 @@ export class ThemeProviderService {
   // style$: Observable<string> = of('.test { background: green; }');
 
   constructor(
+    private route: ActivatedRoute,
     private http: HttpClient
   ) { }
 
   get theme$() {
-    return this.http
-      .get<Theme>('assets/default-theme.json')
-      .pipe(
-        switchMap(t => {
-          if (t.styleUrl) {
-            return this.http
-              .get(t.styleUrl, { responseType: 'text' })
-              .pipe(
-                switchMap(v => from(less.render(v))),
-                map(v => {
-                  t.style = v.css;
-                  return t;
-                }),
-              );
-          } else {
-            return of(t);
-          }
-        })
-      );
-  }
+    const themeUrl$ = this.route.queryParamMap.pipe(map(m => {
+      if (m.has(urlKey)) {
+        return m.get(urlKey);
+      } else {
+        return defaultUrl;
+      }
+    }))
 
-  get style$() {
-    return this.http
-      .get('assets/default-theme.less', { responseType: 'text' })
-      .pipe(
-        switchMap(v => from(less.render(v))),
-        map(v => v.css),
-      );
+    return themeUrl$.pipe(
+      switchMap(themeUrl =>
+        this.http
+          .get<Theme>(themeUrl)
+          .pipe(
+            switchMap(t => {
+              if (t.styleUrl) {
+                return this.http
+                  .get(t.styleUrl, { responseType: 'text' })
+                  .pipe(
+                    switchMap(v => from(less.render(v))),
+                    map(v => {
+                      t.style = v.css;
+                      return t;
+                    }),
+                  );
+              } else {
+                return of(t);
+              }
+            })
+          )
+      )
+    );
   }
 }
